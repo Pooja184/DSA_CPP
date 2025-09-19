5b5bcf97d82b5d31114f661d577737b854a84c33
pxxja <poojabhambid2004@gmail.com>
2025-09-19 20:12:05 +0530

    boilerplate code

---
+++ Day1/boilerplate.cpp
@@ -0,0 +1,8 @@
+#include<iostream> //It tells the compiler to include the input/output stream library before actual compilation
+using namespace std;// allows yout to use names from the standard namespace without writing std:: every time
+
+// This is the starting point of execution
+int main(){
+    cout<<"Pooja"<<endl<<"Bhambid";
+    return 0;
+}
