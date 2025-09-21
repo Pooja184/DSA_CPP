c26a81608c47d3da1cad1d735bd70a6ddcca8bf3
pxxja <poojabhambid2004@gmail.com>
2025-09-21 19:57:46 +0530

    triangle using numbers

--- Day3-patterns/triangleUsingNum.cpp
+++ Day3-patterns/triangleUsingNum.cpp
@@ -3,9 +3,15 @@ using namespace std;
 
 int main(){
     int range=4;
+    // for(int i=1;i<=range;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<i<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
     for(int i=1;i<=range;i++){
         for(int j=1;j<=i;j++){
-            cout<<i<<" ";
+            cout<<j<<" ";
         }
         cout<<"\n";
     }
