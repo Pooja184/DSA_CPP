1b99dd8efe387011c587f04dd983fd246ae3ec79
pxxja <poojabhambid2004@gmail.com>
2025-09-26 15:10:38 +0530

    decimal to binary conversion

---
+++ Day5-BinaryNumberSystem/decimalToBinary.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+int decimalToBinaryConversion(int num){
+    int ans=0,power=1;
+
+    while(num>0){
+        int rem=num%2;
+        num/=2;
+        ans+=(rem*power);
+        power*=10;
+
+    }
+    return ans;
+   
+}
+int main(){
+    int num=42;
+    cout<<decimalToBinaryConversion( num);
+    return 0;
+}
