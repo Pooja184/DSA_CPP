20f6724b33bd7b8d49bb76f42928a0e50a40675f
pxxja <poojabhambid2004@gmail.com>
2025-09-29 19:30:49 +0530

    use min function to calculate smallest number

--- Day7-Arrays/findSmallest.cpp
+++ Day7-Arrays/findSmallest.cpp
@@ -6,9 +6,12 @@ int main(){
     int arr[size]={56,23,98,67,34};
     int smallest=INT_MAX;
     for(int i=0;i<size;i++){
-        if(arr[i]<smallest){
-            smallest=arr[i];
-        }
+        // if(arr[i]<smallest){
+        //     smallest=arr[i];
+        // }
+
+        // here we can also use min function which is implicit 
+       smallest= min(arr[i],smallest);
     }
     cout<<smallest;
 
