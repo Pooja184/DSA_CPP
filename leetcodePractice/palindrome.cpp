268be721c41be852d84333878671ec8abf5a3725
pxxja <poojabhambid2004@gmail.com>
2025-10-06 16:34:17 +0530

    solve palindrome question

---
+++ leetcodePractice/palindrome.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+
+bool palindrome(int x){
+    
+     string str=to_string(x);
+     int i=0,j=str.size()-1;
+    //  cout<<str[i]<<str[j]<<" ";
+
+
+        bool isPalindrome=true;
+        if(j==0){
+            return palindrome;
+        }
+        while(i<j){
+            cout<<str[i]<<" "<<str[j]<<endl;
+            if(str[i]==str[j]){
+                isPalindrome=true;
+            }else{
+                return false;
+            }
+            i++;
+            j--;
+        }
+        if(isPalindrome){
+            return true;
+        }else{
+            return false;
+        }
+}
+
+int main(){
+    int num=1000021;
+    cout<<palindrome(num);
+    return 0;
+}
