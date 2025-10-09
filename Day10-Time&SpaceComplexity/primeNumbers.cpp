7faa46d5553ee8b32c785b83c47800a58a65e2df
pxxja <poojabhambid2004@gmail.com>
2025-10-09 19:53:58 +0530

    make changes in prime function

--- Day10-Time&SpaceComplexity/primeNumbers.cpp
+++ Day10-Time&SpaceComplexity/primeNumbers.cpp
@@ -3,16 +3,22 @@ using namespace std;
 
 
 void primeNumbers(int num){
+    bool primeN=true;
     for(int i=2;i*i<=num;i++){
         if(num%i==0){
-            cout<<"Not Prime";
+            primeN=false;
             break;
         }
     }
-    cout<<"Prime";
+    // cout<<"Prime";
+    if(primeN){
+        cout<<"Prime";
+    }else{
+        cout<<"Not Prime";
+    }
 }
 int main(){
-    int num=7;
+    int num=3;
     primeNumbers(num);
     return 0;
 }
